6909754bdba5ea92c2fa174564ec560fa524fae7
Mathias Poulsen <mathiiaspoulsen@gmail.com>
2018-05-22 23:09:39 +0200

    only continuous dates can be chosen on the calendar

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -6,11 +6,12 @@ function Room(roomId, beds, desc, price, imageSrc) {
   this.imageSrc = imageSrc;
 }
 
-function Reservation(roomId, dates, firstName, lastName) {
+function Reservation(roomId, dates, firstName, lastName, email) {
   this.roomId = roomId;
   this.dates = dates;
   this.firstName = firstName;
   this.lastName = lastName;
+  this.email = email;
 }
 
 function getLocalRooms() {
@@ -70,41 +71,55 @@ function setLocalReservation(origReservations) {
   localStorage.setItem("vbbReservations", newReservations);
 }
 
-function makeReservation(roomId, dates, firstName, lastName) {
+function makeReservation(roomId, dates, firstName, lastName, email) {
   let origReservations = getLocalReservations();
 
-  let newReservation = new Reservation(roomId, dates, firstName, lastName);
+  let newReservation = new Reservation(roomId, dates, firstName, lastName, email);
 
   origReservations.unshift(newReservation);
 
   setLocalReservation(origReservations);
 }
 
+//Function is called when a day on the calendar is clicked
 function daySelect(event) {
-  let date = event.target.textContent;
+  let date = event.target.textContent + "-05" + "-2018";
+  //If day is green, try to remove selection
   if (event.target.style.background === "green") {
-    event.target.style.background = "";
+    //Go on if selectedDates contains chosen date
     if (selectedDates.includes(date)) {
-      selectedDates.splice(selectedDates.indexOf(date), 1);
-      console.log(selectedDates);
+      /*Checks if chosen date is first or last date in selectedDates.
+      Important for making sure that user can not remove dates that are not
+      the first or last of selectedDates.
+      If the chosen date is either the last or first date of selectedDates,
+      the date is removed from the array*/
+      if (!inMiddle(selectedDates, date)) {
+        event.target.style.background = "";
+        selectedDates.splice(selectedDates.indexOf(date), 1);
+        console.log(selectedDates);
+      }
     }
   }
-  else {
+  //If chosen date is neighbor to either the first or last date in selectedDates the chosen date can be added to the array. Important for securing that selectedDates does not contain any holes
+  else if (isNeighbor(selectedDates, date)) {
     event.target.style.background = "green";
     selectedDates.unshift(date);
     selectedDates.sort(function(a, b) {
-      return a - b
+      //Very incomplete... only checks on day. Not month nor year,
+      return a.split("-")[0] - b.split("-")[0];
     });
     console.log(selectedDates);
   }
 }
 
+//clears chosen dates. Used for cleaning after ended reservation
 function clearDates() {
   for (td of document.querySelectorAll("td")) {
     td.style.background = "";
   }
 }
 
+// Selects a room for reservation
 function pickRoom(event) {
   selectedRoom = event.target.dataset.roomid;
 }
@@ -112,14 +127,64 @@ function pickRoom(event) {
 function submitReservation() {
   let firstName = document.querySelector("#fname").value;
   let lastName = document.querySelector("#lname").value;
+  let email = document.querySelector("#email").value;
 
-  makeReservation(selectedRoom, selectedDates, firstName, lastName);
+  makeReservation(selectedRoom, selectedDates, firstName, lastName, email);
 
   document.querySelector("#fname").value = "";
   document.querySelector("#lname").value = "";
   selectedRoom = "";
   selectedDates = [];
   clearDates();
+}
+
+//Checks if item is equal to the first or the last item in the array. If not it is assumed that the item is somewhere in the middle of the array. Because of that it is needed to check that the array contains the item, if that information is needed.
+function inMiddle(array, item) {
+  let answer = true;
+  if (array[0] === array[array.indexOf(item)] || array[array.length - 1] === array[array.indexOf(item)]) {
+    answer = false;
+  }
+
+  return answer;
+}
+
+//Checks if item is neighbor to either the first or the last item in the array
+function isNeighbor(array, item) {
+  let answer = false;
+
+  if (!array.includes(item)) {
+    if (array.length != 0) {
+      let firstDay = array[0].split("-")[0];
+      let firstMonth = array[0].split("-")[1];
+      let firstYear = array[0].split("-")[2];
+      let lastDay = array[array.length-1].split("-")[0];
+      let lastMonth = array[array.length-1].split("-")[1];
+      let lastYear = array[array.length-1].split("-")[2];
+
+      let itemDay = item.split("-")[0];
+      let itemMonth = item.split("-")[1];
+      let itemYear = item.split("-")[2];
+
+      if (firstYear == itemYear) {
+        if (firstMonth == itemMonth) {
+          if ((firstDay == parseInt(itemDay) + 1) || firstDay == parseInt(itemDay) - 1) {
+            answer = true;
+          }
+        }
+      }
+      if (lastYear == itemYear) {
+        if (lastMonth == itemMonth) {
+          if ((lastDay == parseInt(itemDay) + 1) || lastDay == parseInt(itemDay) - 1) {
+            answer = true;
+          }
+        }
+      }
+    }
+    else {
+      answer = true;
+    }
+  }
+  return answer;
 }
 
 var selectedDates = [];
@@ -135,6 +200,7 @@ window.onload = function() {
 
   days = document.querySelectorAll("td");
 
+  //adds eventlisteners for all dates in the calendar
   for (day of days) {
     day.addEventListener("click", function() {
       daySelect(event);
