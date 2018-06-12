61151b0d55eb742a47f7d841ce3d73f6dadccb4b
Ditte Koustrup <Ditte.j.kk1994@hotmail.com>
2018-06-12 18:13:13 +0200

    improvements

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -12,18 +12,13 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.querySelector('.close')[0];
+var span = document.querySelector('.close');
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = 'none';
 }
-}
 
-// Get the modal
-var modal = document.querySelector('#myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.querySelector('#myImg-1');
 var modalImg = document.querySelector('#img01');
 var captionText = document.querySelector('#caption');
@@ -31,12 +26,4 @@ img.onclick = function(){
     modal.style.display = 'block';
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.querySelector('.close')[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = 'none';
 }
